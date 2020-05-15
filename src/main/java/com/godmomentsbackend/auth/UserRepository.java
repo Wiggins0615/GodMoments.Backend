f3736c160c252ce8c1abfdfbc942b1fe4552435c
Wiggins0615 <Wigginscryan0615@outlook.com>
2020-05-14 22:13:52 -0500

    Latest

--- src/main/java/com/godmomentsbackend/auth/UserRepository.java
+++ src/main/java/com/godmomentsbackend/auth/UserRepository.java
@@ -1,5 +1,8 @@
 package com.godmomentsbackend.auth;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	User findByUsername(String username);
 
 }
